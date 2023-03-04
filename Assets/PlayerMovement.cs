a756453fb3d96376f7505b70ccb0903d576bb7e7
Goku0101 <113784414+Goku0101@users.noreply.github.com>
2023-03-04 11:00:42 +0000

    Added Player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Movement
+
+    CharacterController controller;
+
+    Vector3 move;
+
+    public float speed = 5f;
+
+    //Gravity + physics
+
+    Vector3 velocity;
+
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+
+    public LayerMask groundMask;
+
+    public float groundDistance = 0.4f;
+
+    bool isGrounded;
+
+    public float jumpHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+
+        float z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            velocity.y = gravity * jumpHeight * -2f * Time.deltaTime;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
